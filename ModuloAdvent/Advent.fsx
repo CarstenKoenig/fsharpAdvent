848b1832b8513d8659f7c37ed97e108158c207df
Carsten König <Carsten@gettingsharper.de>
2014-12-18 11:49:16 +0100

    prepare for github

--- ModuloAdvent/Advent.fsx
+++ ModuloAdvent/Advent.fsx
@@ -1,17 +1,16 @@
-﻿
-(*
+﻿(*
                                                        *
      *                                                          *
                                   *                  *        .--.
       \/ \/  \/  \/                                        ./   /=*
         \/     \/      *            *                ...  (_____)
-         \ ^ ^/                                       \ \_((^o^))-.     *
+         \ ^ ^/                                       \ \_((O.O))-.     *
          (o)(O)--)--------\.                           \   (   ) \  \._.
          |    |  ||================((~~~~~~~~~~~~~~~~~))|   ( )   |     \
           \__/             ,|        \. * * * * * * ./  (~~~~~~~~~~~)    \
    *        ||^||\.____./|| |          \___________/     ~||~~~~|~'\____/ *
-            || ||     || || A            ||    ||          ||    |   jurcy
-     *      <> <>     <> <>          (___||____||_____)   ((~~~~~|   *
+            || ||     || || V            ||    ||         ||    |   
+     *      <> <>     <> <>          (___||____||_____)  ((~~~~~|   *
 
 *)
 
@@ -26,9 +25,14 @@ type numbers = Numbers.NumbersProvider<"4,5">
 type N5 = numbers.N5
 type N4 = numbers.N4
 
+let modN<'n when 'n :> INum and 'n : (new : unit -> 'n)> x =
+    Mod<'n>.create x
+
 let modF<'p when 'p :> IPrime and 'p : (new : unit -> 'p)> x =
     ModF<'p>.create x
 
+// be nice and work this out please
+printfn "4 + 3 * 2 = %A" (modN 4 + modN 3 * modN 2 : Mod<N5>)
 
 // ho ho ho ... divide like rudolph
 printfn "4 / 3 = %A" (modF 4 / modF 3 : ModF<N5>)
